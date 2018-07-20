eb43a92a991102971e1549b94eb1a1908ba67bb5
zhengguang666 <582635492@qq.com>
2018-07-20 17:32:17 +0800

    follow imx6, mv boottimes file to /etc

--- LAB/boot_count/S8800.sh
+++ LAB/boot_count/S8800.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-ls /mnt/boottimes > /dev/zero 2>&1
+ls /etc/boottimes > /dev/zero 2>&1
 if [ $? -eq 0 ]; then
-   boots=`cat /mnt/boottimes`
+   boots=`cat /etc/boottimes`
    boots=$[ boots+1 ]
 else
    boots=1
 fi
    echo "boots=$boots"
-   echo "$boots" > /mnt/boottimes
+   echo "$boots" > /etc/boottimes
 sync
 
